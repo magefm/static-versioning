37065ec84e1aca3f27758ec32f832df01bebd65c
Rafael Kassner <kassner@gmail.com>
2014-06-04 10:50:43 -0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-static-versioning
+Static Versioning
 =================
+
+This module appends the MD5 of the file in the URL of static files like CSS and JS files. With this, when you deploy changes to CSS or JS files, the browser will receive the brand new version of the file without the need to reset the cache.
---
+++ app/code/community/MageFM/StaticVersioning/Block/Page/Html/Head.php
@@ -0,0 +1,50 @@
+<?php
+
+class MageFM_StaticVersioning_Block_Page_Html_Head extends Mage_Page_Block_Html_Head
+{
+
+    protected function &_prepareStaticAndSkinElements($format, array $staticItems, array $skinItems, $mergeCallback = null)
+    {
+        $designPackage = Mage::getDesign();
+        $baseJsUrl = Mage::getBaseUrl('js');
+        $items = array();
+        if ($mergeCallback && !is_callable($mergeCallback)) {
+            $mergeCallback = null;
+        }
+
+        // get static files from the js folder, no need in lookups
+        foreach ($staticItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $items[$params][] = $mergeCallback ? Mage::getBaseDir() . DS . 'js' . DS . $name : $baseJsUrl . $name . '?v=' . md5_file(Mage::getBaseDir() . DS . 'js' . DS . $name);
+            }
+        }
+
+        // lookup each file basing on current theme configuration
+        foreach ($skinItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $items[$params][] = $mergeCallback ? $designPackage->getFilename($name, array('_type' => 'skin')) : $designPackage->getSkinUrl($name, array()) . '?v=' . md5_file($designPackage->getFilename($name, array('_type' => 'skin')));
+            }
+        }
+
+        $html = '';
+        foreach ($items as $params => $rows) {
+            // attempt to merge
+            $mergedUrl = false;
+            if ($mergeCallback) {
+                $mergedUrl = call_user_func($mergeCallback, $rows);
+            }
+            // render elements
+            $params = trim($params);
+            $params = $params ? ' ' . $params : '';
+            if ($mergedUrl) {
+                $html .= sprintf($format, $mergedUrl, $params);
+            } else {
+                foreach ($rows as $src) {
+                    $html .= sprintf($format, $src, $params);
+                }
+            }
+        }
+        return $html;
+    }
+
+}
